71a97af62fe9078087d0989534336a1ef8a22f80
snowplushie <snowplushie@yahoo.com>
2019-04-28 05:01:39 -0400

    first commit

---
+++ lib/billboard/cli.rb
@@ -0,0 +1,8 @@
+require "billboard/cli/version"
+
+module Billboard
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/billboard/cli/version.rb
@@ -0,0 +1,5 @@
+module Billboard
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
